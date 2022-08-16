8a19e9b98e3ee0bfcae2af042fb1d6a6e0c783a9
FranLagorio <franlagorio@gmail.com>
2022-08-15 23:54:07 -0300

    Desafio N°6

--- Clase 6/public/index.js
+++ Clase 6/public/index.js
@@ -6,23 +6,95 @@ const socket = io();
 //El socket es uno, es la conexion
 //El canal es el socket
 socket.on("connect", () => {
-	console.log("me conecte");
+  console.log("Te has conectado");
 });
 
-socket.on("data-generica", (data) => {
-	console.log(data);
-});
+// socket.on("data-generica", (data) => {
+//   console.log(data);
+// });
+
+//le mando la el mensaje
+function enviarChat() {
+  //socket.emit("data-generica", "Hace 4 segundos que estas conectado");
+  const email = document.getElementById("caja-nombre").value;
+  const mensaje = document.getElementById("caja-msg").value;
+
+  var d = new Date(),
+    dformat =
+      [d.getMonth() + 1, d.getDate(), d.getFullYear()].join("/") +
+      " " +
+      [d.getHours(), d.getMinutes(), d.getSeconds()].join(":");
+
+  if (email.includes("@" && ".com")) {
+    socket.emit(
+      "data-chat",
+      `<span style="color:blue">${email}</span> <span style="color:red">[${dformat}]</span>: <span style="color:green">${mensaje}</span>`
+    );
+  } else {
+    alert("Debe ingresar un email con el formato ...@--email.com");
+  }
+
+  document.getElementById("caja-msg").value = "";
+}
+// <span color="blue">${item.email}</span>
+//<span color="red">${item.dformat}</span>:
+//<span color="green">${item.mensaje}</span>
+
+//viene el mensaje filtrado
 
 socket.on("arr-chat", (data) => {
-	console.log(data);
-	const html = data.reduce((html, item) => `<div>${item}</div>` + html, "");
-	document.getElementById("div-chats").innerHTML = html;
+  //console.log(data);
+  const html = data.reduce((html, item) => `<div>${item}</div>` + html, "");
+  document.getElementById("div-chats").innerHTML = html;
 });
 
-function enviar() {
-	//socket.emit("data-generica", "Hace 4 segundos que estas conectado");
-	const nombre = document.getElementById("caja-nombre").value;
-	const mensaje = document.getElementById("caja-msg").value;
+//Envio de Form
+function submitProduct(e) {
+  e.preventDefault();
+  const bodyForm = document.getElementById("formProduct");
 
-	socket.emit("data-generica", nombre + " dice " + mensaje);
+  let newProduct = {
+    id: bodyForm.id.value,
+    title: bodyForm.title.value,
+    price: bodyForm.price.value,
+    thumbnail: bodyForm.thumbnail.value,
+  };
+
+  //console.log(newProduct);
+  socket.emit("new-product", newProduct);
+
+  // let inputs = bodyForm.getElementsByTagName("input");
+
+  // let inputList = Array.prototype.slice.call(inputs);
+  // let inputList = Array.from(inputs);
+  // let inputList = [...inputs];
+
+  // inputList.forEach((element) => {
+  //   console.log(element);
+  //   element.value = "";
+  // });
+
+  bodyForm.reset();
+
+  return false;
 }
+
+socket.on("newProducts", (data) => {
+  document.getElementById("productsTable").innerHTML = "";
+  let newProductsTable = "";
+  data.forEach((obj) => {
+    newProductsTable += `
+		<tr>
+		<th scope="row">
+				${obj.id}
+		</th>
+		<td style="text-transform: uppercase;">
+				${obj.title}
+		</td>
+		<td> ${obj.price}
+		</td>
+		<td><img src="${obj.thumbnail}" class="product-img" alt="..." /></td>
+	</tr>`;
+  });
+  document.getElementById("productsTable").innerHTML = newProductsTable;
+});
--- Clase 6/server.js
+++ Clase 6/server.js
@@ -1,37 +1,124 @@
-//https://socket.io/docs/v4/server-initialization/
 const express = require("express");
 const app = express();
+const { engine } = require("express-handlebars");
+const PORT = 8080;
 
-//IMPLEMENTACION
+//IMPLEMENTACION DE SOCKETS
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
-httpServer.listen(process.env.PORT || 8080, () => console.log("SERVER ON"));
+// const server = app.listen(PORT, () => {
+//   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+// });
+// server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
+//INICIO SERVIDOR HTTP
+httpServer.listen(process.env.PORT || PORT, () => console.log("SERVER ON"));
+httpServer.on("error", (error) => console.log(`Error en servidor ${error}`));
+
+//
+app.use("/public", express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
+// Configuracion del motor HANDLEBARS
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+//ARRAY DE PRODUCTOS
+let productsHC = [
+  {
+    id: 1,
+    title: "nike ball",
+    price: 101,
+    thumbnail: "http://localhost:8080/public/nike-ball.jpg",
+  },
+  {
+    id: 2,
+    title: "nike shoes",
+    price: 102,
+    thumbnail: "http://localhost:8080/public/nike-shoes.jpg",
+  },
+  {
+    id: 3,
+    title: "adidas shoes",
+    price: 102,
+    thumbnail: "http://localhost:8080/public/adidas-shoes.jpg",
+  },
+];
+
 app.get("/", (req, res) => {
-	res.sendFile("index.html", { root: __dirname });
+  // res.sendFile("index.html", { root: __dirname });
+  res.render("indexChat", { products: productsHC, productsExist: true });
+});
+
+app.get("/form", (req, res) => {
+  //sirve productslist.hbs en index.hbs (index.hbs es la plantilla por defecto donde arranca todo)
+  res.render("form");
+});
+
+app.get("/products", (req, res) => {
+  if (productsHC.length !== 0) {
+    res.render("productslist", { products: productsHC, productsExist: true });
+  } else {
+    res.render("error", { errorMessage: "No se encuentran productos" });
+  }
+  //sirve productslist.hbs en index.hbs (index.hbs es la plantilla por defecto donde arranca todo)
+});
+
+app.get("/products/:id", (req, res) => {
+  let { id } = req.params;
+  try {
+    let found = productsHC.find((element) => element.id == id);
+    if (found) {
+      res.render("oneProduct", { product: found, title: "Detalle de Producto" });
+    } else {
+      res.render("error", { errorMessage: "Producto no encontrado" });
+    }
+  } catch (error) {}
+  //sirve productslist.hbs en index.hbs (index.hbs es la plantilla por defecto donde arranca todo)
+  // res.render('productslist', { products: productsHC, productsExist: true });
+});
+
+app.post("/", (req, res) => {
+  const { body } = req;
+  productsHC.push(body);
+});
+
+app.post("/products", (req, res) => {
+  const { body } = req;
+  productsHC.push(body);
+  res.render("productslist", { products: productsHC, productsExist: true });
 });
 
 let chat = [];
 
 io.on("connection", (socket) => {
-	console.log("Usuario Conectado" + socket.id);
+  console.log("Usuario Conectado" + socket.id);
+  chat.push("se unio al chat " + socket.id);
+  io.sockets.emit("arr-chat", chat);
 
-	chat.push("se union al chat " + socket.id);
-	io.sockets.emit("arr-chat", chat);
+  // setTimeout(() => {
+  //   socket.emit("data-generica", "Hace 4 segundos que estas conectado");
+  // }, 4000);
 
-	setTimeout(() => {
-		socket.emit("data-generica", "Hace 4 segundos que estas conectado");
-	}, 4000);
+  socket.on("data-chat", (data) => {
+    // data = data.replace("river", "river no! boca");
+    chat.push(data);
+    io.sockets.emit("arr-chat", chat);
+  });
 
-	socket.on("data-generica", (data) => {
-		data = data.replace("river", "river no! boca");
-
-		chat.push(data);
-		io.sockets.emit("arr-chat", chat);
-	});
+  socket.on("new-product", (data) => {
+    productsHC.push(data);
+    io.sockets.emit("newProducts", productsHC);
+  });
 });
