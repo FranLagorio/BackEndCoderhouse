6367410032b4be37868d60ffef83f7116ea6c24f
FranLagorio <franlagorio@gmail.com>
2022-10-04 21:43:53 -0300

    desafio sessions

--- Clase 12 - cookies -desafio/routes/homeRouter.js
+++ Clase 12 - cookies -desafio/routes/homeRouter.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 const homeRouter = Router();
 import { loginController } from "../controller/loginController.js";
 
-homeRouter.use(loginController.auth);
+// homeRouter.use(loginController.auth);
 
-homeRouter.get("/", (req, res, next) => {
-  res.render("pages/home", { name: req.session.user });
+homeRouter.get("/", loginController.auth, (req, res, next) => {
+  res.render("pages/home", { name: req.session.username });
 });
 
 export default homeRouter;
