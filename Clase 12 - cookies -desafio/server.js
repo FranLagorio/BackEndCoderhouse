3f089d11e7c0459eca61cf2248db55ac47212c46
FranLagorio <franlagorio@gmail.com>
2022-10-04 21:43:53 -0300

    desafio sessions

--- Clase 12 - cookies -desafio/server.js
+++ Clase 12 - cookies -desafio/server.js
@@ -4,14 +4,15 @@ import { Server } from "socket.io";
 import session from "express-session";
 import { homeRouter, productRouter, loginRouter } from "./routes/index.js";
 import { socketController } from "./src/utils/socketController.js";
+import MongoStore from "connect-mongo";
 
 //Creacion de Servidor y Sockets
 const app = express();
 const PORT = process.env.port || 8080;
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 socketController(io);
-
 //Inicio de Servidor
 httpServer.listen(process.env.PORT || PORT, () =>
   console.log("Servidor Funcionando en Puerto: " + PORT)
@@ -32,15 +33,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Configuracion de Session
+// app.use(
+//   session({
+//     secret: "secreto",
+//     cookie: { maxAge: 600000 },
+//     //resave viene de la documentacion
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
 app.use(
   session({
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://franchas123:fran123@cluster0.zqkvn9v.mongodb.net/?retryWrites=true&w=majority",
+      mongoOptions: advancedOptions,
+    }),
     secret: "secreto",
     cookie: { maxAge: 600000 },
     //resave viene de la documentacion
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
   })
 );
+
 app.use((req, res, next) => {
   req.session.touch();
   next();
@@ -55,7 +72,7 @@ app.use("/login", loginRouter);
 app.use("/home", homeRouter);
 
 app.get("/logout", (req, res) => {
-  let username = req.session.user;
+  let username = req.session.username;
 
   req.session.destroy((err) => {
     if (err) {
